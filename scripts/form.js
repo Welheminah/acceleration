4479d808594a577e88a6ce90adb78f61842738cc
Welheminah Lehutso <welheminah.lehutso>
2022-08-30 13:13:33 +0200

    first commit

---
+++ scripts/form.js
@@ -0,0 +1,10 @@
+function validateForm() {
+    var formInputs = document.forms["application-form"]["full name"]["Email address"].value;
+    if (formInputs == "") {
+      alert("all fields must be filled out");
+      return false;
+    }
+  }
+
+  
+
